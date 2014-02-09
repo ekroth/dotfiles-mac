3044f5c462fd87eede8b02518e7440a41ce9333a
Andrée Ekroth <andree.ekroth@gmail.com>
2014-02-09 12:00:37 +0100

    various

--- xmonad.d/xmonad.hs
+++ xmonad.d/xmonad.hs
@@ -6,24 +6,29 @@ import XMonad.Util.EZConfig(additionalKeys)
 
 import qualified XMonad.Hooks.EwmhDesktops as EWMH
 
+import XMonad.Hooks.ManageDocks(avoidStruts)
+import XMonad.Layout.NoBorders(smartBorders)
+
 main = xmonad $ EWMH.ewmh xfceConfig
-         { terminal = "xfce4-terminal"
+         { terminal = "urxvt"
          , modMask = mod4Mask
 	 , manageHook = myManageHook <+> manageHook xfceConfig
 	 , workspaces = myWorkspaces
 	 , handleEventHook = handleEventHook xfceConfig <+> EWMH.fullscreenEventHook
+	 , layoutHook = avoidStruts $ smartBorders $ layoutHook xfceConfig 
          } `additionalKeys` myKeys
 
 myWorkspaces = [ "term", "web", "code", "other" ]
 
 myManageHook = composeAll
 	[ className =? "Firefox-bin" --> doShift "web"
+	, className =? "Xfce4-notifyd" --> doIgnore
 	, className =? "xfce4-terminal" --> doShift "term"
 	, className =? "Thunar" --> doShift "other"
 	, (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat ]
 
 myKeys = 
-       [ ((mod4Mask,				xK_period),  nextWS)
-	,((mod4Mask,			    	xK_comma),   prevWS)
-	,((mod4Mask .|. shiftMask, 		xK_period),  shiftToNext >> nextWS)
-	,((mod4Mask .|. shiftMask, 		xK_comma),   shiftToPrev >> prevWS) ]
+	[ ((mod4Mask,				xK_period),  nextWS)
+	, ((mod4Mask,			    	xK_comma),   prevWS)
+	, ((mod4Mask .|. shiftMask, 		xK_period),  shiftToNext >> nextWS)
+	, ((mod4Mask .|. shiftMask, 		xK_comma),   shiftToPrev >> prevWS) ]
--- zshrc
+++ zshrc
@@ -9,7 +9,7 @@ SAVEHIST=1000
 bindkey -e
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
-zstyle :compinstall filename '/home/glakke/.zshrc'
+zstyle :compinstall filename "$HOME/.zshrc"
 
 autoload -Uz compinit
 compinit
@@ -20,8 +20,8 @@ compinit
 #------------------------------
 PATH=$HOME/bin:$HOME/Dropbox/linux/script:/tools:/opt/android-sdk:/usr/bin/vendor_perl:/opt/kde/bin:${PATH}
 
-export TERM=xterm-256color
-export EDITOR='emacs -nw'
+export TERM=urxvt
+export EDITOR='emacs'
 export BROWSER=/usr/bin/chromium
 export ANDROID_HOME=/opt/android-sdk
 export ANDROID_NDK_ROOT=/opt/android-ndk
@@ -37,11 +37,10 @@ alias ls='ls --color=auto -h'
 alias ntop='nethogs'
 alias top='htop'
 alias twat='mkdir wat && cd wat'
-alias em='emacs -nw'
+
 # SSH
 alias evalssh='eval `keychain --agents ssh --eval id_rsa` || exit 1'
 alias lthssh='ssh dat11aek@login.student.lth.se'
 alias lthsshfs='sshfs dat11aek@login.student.lth.se: /media/ssh/lth'
 alias android-connect="mtpfs -o allow_other /media/android"
 alias android-disconnect="fusermount -u /media/android"
-alias sbt-kill='ps -ef | grep java | grep "'".sbt/boot"'" | awk "'"{print $2}"'" | xargs kill || echo "NOTHING TO KILL!"'
