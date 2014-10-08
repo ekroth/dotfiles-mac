2609b2dc6f1576d3f38a6d846b850ea067f76dd6
Andrée Ekroth <andree.ekroth@gmail.com>
2014-10-08 16:35:52 +0200

    create-grub fix

--- bin/create-grub.sh
+++ bin/create-grub.sh
@@ -5,7 +5,16 @@ set -e
 echo "generate grub.cfg.."
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "mount macbook.."
+sudo mount -t hfsplus -o force,rw /dev/sda4 /mnt
+
+echo "backup old boot.efi.."
+sudo cp -f /mnt/System/Library/CoreServices/boot.efi /mnt/System/Library/CoreServices/boot.efi.backup
+
 echo "generate boot.efi.."
-sudo grub-mkstandalone -o boot.efi -d /usr/lib/grub/x86_64-efi -O x86_64-efi --compress=xz /boot/grub/grub.cfg
+sudo grub-mkstandalone -o /mnt/System/Library/CoreServices/boot.efi -d /usr/lib/grub/x86_64-efi -O x86_64-efi --compress=xz /boot/grub/grub.cfg
+
+echo "unmount macbook.."
+sudo umount /mnt
 
 echo "done"
